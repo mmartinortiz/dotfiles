aa991af4455676a1b1e5e60c53978333a6795810
mmartinortiz <mmartinortiz@gmail.com>
2025-05-18 22:09:41 +0200

    Fix syntax + format

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -120,18 +120,18 @@ return {
     -- Status column plugin that provides a configurable 'statuscolumn' and click handlers.
     "luukvbaal/statuscol.nvim",
     config = function()
-      -- local builtin = require("statuscol.builtin")
+      local builtin = require("statuscol.builtin")
       require("statuscol").setup({
-          setopt = true,
-  relculright = true,
-  segments = {
-    { text = { '%s' }, click = 'v:lua.ScSa' },
-    {
-      text = { builtin.lnumfunc, ' ' },
-      condition = { true, builtin.not_empty },
-      click = 'v:lua.ScLa',
-    },
-  },
+        setopt = true,
+        relculright = true,
+        segments = {
+          { text = { '%s' }, click = 'v:lua.ScSa' },
+          {
+            text = { builtin.lnumfunc, ' ' },
+            condition = { true, builtin.not_empty },
+            click = 'v:lua.ScLa',
+          },
+        },
 
       })
     end
