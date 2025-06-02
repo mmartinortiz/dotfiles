5d565a6db04616f1209f21c6d130d37da322de55
Manuel <mmartinortiz@gmail.com>
2025-06-02 10:06:10 +0200

    Include completions provided by brew

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -104,3 +104,11 @@ eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 batman --export-env | source
 eval (batpipe)
+
+# Include completions provided by brew
+if test -d (brew --prefix)/share/fish/completions
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+if test -d (brew --prefix)/share/fish/vendor_completions.d
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
