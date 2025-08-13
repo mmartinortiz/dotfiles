d4423def44f7250d1433617eacf917e949c16cd7
Manuel <mmartinortiz@gmail.com>
2025-08-13 09:56:15 +0200

    Remove legacy file

--- private_dot_config/tmux/executable_start_tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if the tmux session "zero" exists
-if ! tmux has-session -t zero 2>/dev/null; then
-    # Start a new tmux session named "zero"
-    tmux new-session -d -s zero
-fi
-
