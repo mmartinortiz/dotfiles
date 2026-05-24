a06b5841fc08f5efb8e66ccb2ad9fc27c050b96c
Manuel <mmartinortiz@gmail.com>
2026-05-24 17:17:26 +0200

    feat: Format markdown, json and yaml on save with prettier

---
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      json = { "prettier" },
+      json5 = { "prettier" },
+      jsonc = { "prettier" },
+      markdown = { "prettier" },
+      ["markdown.mdx"] = { "prettier" },
+      mdx = { "prettier" },
+      yaml = { "prettier" },
+    },
+  },
+}
